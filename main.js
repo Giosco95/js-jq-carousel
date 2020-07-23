2046e3d16bb04cc23909445040f630d2f92976d3
Giorgio Scollo <giosco1995@gmail.com>
2020-07-23 21:50:26 +0200

    no bonus

--- main.js
+++ main.js
@@ -15,15 +15,15 @@ $(document).ready(function(){
    var activeImage = $('.images img.active');
    var dot = $('.nav i.active')
 
-   //tolgo classe//
+   //tolgo la cera//
    activeImage.removeClass('active');
    dot.removeClass('active');
 
-   //mettere classe next//
+   //metto la cera//
    if(activeImage.hasClass('last')==true){
    $('.images img.first').addClass('active');
    $('.nav i.first').addClass('active');
-   }else{
+   }else {
     activeImage.next().addClass('active');
     dot.next().addClass('active');
     }
@@ -33,9 +33,12 @@ $(document).ready(function(){
   function prevImage(){
    var activeImage = $('.images img.active');
    var dot = $('.nav i.active');
+
+   //tolgo la cera
    activeImage.removeClass('active');
    dot.removeClass('active');
 
+   //metto la cera
    if(activeImage.hasClass('first')){
    $('.images img.last').addClass('active');
    $('.nav i.last').addClass('active');
